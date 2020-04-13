6bc4a0519e9dc523a9a6e4644d38832086274ebc
Nori <nori755@gmail.com>
2020-04-13 17:16:55 +0900

    Refact view.

--- flask_blog/views/entries.py
+++ flask_blog/views/entries.py
@@ -2,7 +2,7 @@ from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
 
 @app.route('/')
-def show_etnries():
+def show_entries():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
     return render_template('entries/index.html')
