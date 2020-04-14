f7db8f40dc0b99de84d0a10ec44755b57a76c00b
Nori <nori755@gmail.com>
2020-04-14 17:42:25 +0900

    Add decorator.

--- flask_blog/views/entries.py
+++ flask_blog/views/entries.py
@@ -1,24 +1,22 @@
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app, db
 from flask_blog.models.entries import Entry
+from flask_blog.views.views import login_required
 
 @app.route('/')
+@login_required
 def show_entries():
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     entries = Entry.query.order_by(Entry.post_id.desc()).all()
     return render_template('entries/index.html', entries=entries)
 
 @app.route('/entries/new', methods=['GET'])
+@login_required
 def new_entry():
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     return render_template('entries/new.html')
 
 @app.route('/entries', methods=['POST'])
+@login_required
 def add_entry():
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     entry = Entry(
         title = request.form['title'],
         text = request.form['text']
@@ -29,23 +27,20 @@ def add_entry():
     return redirect(url_for('show_entries'))
 
 @app.route('/entries/<int:post_id>', methods=['GET'])
+@login_required
 def show_entry(post_id):
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     entry = Entry.query.get(post_id)
     return render_template('entries/show.html', entry=entry)
 
 @app.route('/entries/<int:post_id>/edit', methods=['GET'])
+@login_required
 def edit_entry(post_id):
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     entry = Entry.query.get(post_id)
     return render_template('entries/edit.html', entry=entry)
 
 @app.route('/entries/<int:post_id>/update', methods=['POST'])
+@login_required
 def update_entry(post_id):
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     entry = Entry.query.get(post_id)
     entry.title = request.form['title']
     entry.text = request.form['text']
@@ -56,9 +51,8 @@ def update_entry(post_id):
 
 
 @app.route('/entries/<int:post_id>/delete', methods=['POST'])
+@login_required
 def delete_entry(post_id):
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     entry = Entry.query.get(post_id)
     db.session.delete(entry)
     db.session.commit()
